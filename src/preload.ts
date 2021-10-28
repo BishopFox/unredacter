80dbf0df354ee4dd04188f7874162e965b14e4c5
Dan Petro <dpetro@bishopfox.com>
2021-10-28 09:42:22 -0700

    Major memory issue, but recursion works!

--- src/preload.ts
+++ src/preload.ts
@@ -2,6 +2,10 @@
 // It has the same sandbox as a Chrome extension.
 const { ipcRenderer } = require('electron');
 
+const guessable_characters = 'abcdefghijklmnopqrstuvwxyz ';
+// TODO HARDCODED TOTAL LENGTH
+const max_length = 20;
+
 window.addEventListener("DOMContentLoaded", () => {
   const replaceText = (selector: string, text: string) => {
     const element = document.getElementById(selector);
@@ -24,22 +28,44 @@ process.once('loaded', () => {
 
     if (message.command === 'start-redacting') {
 
-      // Do a depth-first search
       var guess = "t";
-      var guessable_characters = 'abcdefghijklmnopqrstuvwxyz ';
-
-      // Depth first search
-      for (let i = 0; i < guessable_characters.length; i++) {
-        var score = await makeGuess(guess + guessable_characters[i]);
-        console.log(guess + guessable_characters[i], score);
-      }
+      // Initial call to the recursive function
+      var score = await guessRecursive(guess);
     }
   });
 });
 
+async function guessRecursive(guess: string) {
+
+  // First, make a direct guess for all characters appended on
+  if (guess.length === max_length) {
+    return;
+  }
+  var scores = new Map();
+
+  for (let i = 0; i < guessable_characters.length; i++) {
+    var result = await makeGuess(guess + guessable_characters[i]);
+    console.log("score: ", result);
+    // Discard bad scores
+    if (result.score < 0.5) {
+      scores.set(result.score, guess + guessable_characters[i]);
+    }
+  }
+
+  // Now sort the results by score
+  var mapAsc = new Map([...scores.entries()].sort());
+
+  // Do the whole thing again for each of these new guesses
+  for (const entry of mapAsc.entries()) {
+    const score = entry[0];
+    const guess = entry[1];
+    await guessRecursive(guess);
+  }
+}
+
 async function makeGuess(guess: string) {
   // TODO HARDCODED TOTAL LENGTH
-  const request = {command: "start-redacting", totalLength: 20, text: guess}
+  const request = {command: "start-redacting", totalLength: max_length, text: guess}
   const result = await ipcRenderer.invoke('redact', request);
   // Send back to DOM to display results on page
   window.postMessage(result, "file://");
